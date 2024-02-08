0cf6dcc1b00b1c875087f0d291137056b5f4acfb
Hadi <hadi.khouja@uni-wuppertal.de>
2024-02-08 11:33:20 +0100

    Using UserService to fetch User Rights

--- policy-app/src/app/components/users-managment/users-managment.component.ts
+++ policy-app/src/app/components/users-managment/users-managment.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { Observable, map, tap } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 import { OpaFetchService } from 'src/app/services/opa-fetch.service';
@@ -17,14 +17,11 @@ interface Policy {
   templateUrl: './users-managment.component.html',
   styleUrls: ['./users-managment.component.scss'],
 })
-export class UsersManagmentComponent implements OnInit {
+export class UsersManagmentComponent {
   public PolicyData!: Observable<Policy>;
+  user = this.userService.activeUser;
 
-  allowedUserRights: Right[] = [
-    { action: 'create', allow: 'allow' },
-    { action: 'edit', allow: 'allow' },
-    { action: 'delete', allow: 'undefined' },
-  ];
+  allowedUserRights: Right[] = [];
 
   allowedGroupRights: Right[] = [
     { action: 'create', allow: 'denied' },
@@ -36,14 +33,16 @@ export class UsersManagmentComponent implements OnInit {
     private userService: UserService,
     private http: HttpClient,
     private opa: OpaFetchService,
-  ) {}
+  ) {
+    effect(() => this.fetchData())
+  }
 
-  ngOnInit(): void {
+  fetchData() {
     let body = {
       input: {
         user: {
           username: 'ad',
-          roles: ['developer', 'admin'],
+          roles: [this.user()],
         },
       },
     };
@@ -52,9 +51,15 @@ export class UsersManagmentComponent implements OnInit {
       .post<any>(window.location.origin + '/opa/v1/data/user_managment/users', body)
       .pipe(map((value) => value.result),
       tap((value: Policy) => {
-        this.allowedUserRights[0].allow = value.create;
-        this.allowedUserRights[1].allow = value.edit;
-        this.allowedUserRights[2].allow = value.delete;
+        if(this.allowedUserRights.length === 0) {
+          this.allowedUserRights.push({ action: 'create', allow: value.create});
+          this.allowedUserRights.push({ action: 'edit', allow: value.edit});
+          this.allowedUserRights.push({ action: 'delete', allow: value.delete});
+        } else {
+          this.allowedUserRights[0] = { action: 'create', allow: value.create};
+          this.allowedUserRights[1] = { action: 'edit', allow: value.edit};
+          this.allowedUserRights[2] = { action: 'delete', allow: value.delete};
+        }
       }));
   }
 }
