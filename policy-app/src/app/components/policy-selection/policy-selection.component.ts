bc357f16344191824a2d67b2c5eb34bc5eb377d6
Hadi <hadi.khouja@uni-wuppertal.de>
2024-03-13 13:31:33 +0100

    policy Selection site works

--- policy-app/src/app/components/policy-selection/policy-selection.component.ts
+++ policy-app/src/app/components/policy-selection/policy-selection.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { SidenavService } from 'src/app/services/sidenav.service';
 
 @Component({
   selector: 'policy-selection',
   templateUrl: './policy-selection.component.html',
-  styleUrl: './policy-selection.component.scss'
+  styleUrl: './policy-selection.component.scss',
 })
 export class PolicySelectionComponent {
+  constructor(private sidenav: SidenavService) {}
 
+  public updatePolicy() {
+    this.close();
+  }
+
+  public close() {
+    this.sidenav.close();
+  }
 }
